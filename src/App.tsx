aa2e4d746c453ab9172e9dd7f09632709c45dc33
WolfSoul <alejandrof011021@gmail.com>
2023-01-31 10:09:23 -0500

    Se creo el routin y Modifico el App -Comp-App fixes #2

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
-function App(){
-  return(
-    <h1>Hola mundo </h1>
+import './App.css'
+import { Link, Route, Switch } from 'react-router-dom';
+import Home from './Componentes/Home';
+import Convertor from './Componentes/Convertor';
+
+function App() {
+  return (
+    <div className="App">
+      <nav>
+        <div className="menu">
+          <Link to="/">Home</Link>
+          <Link to="/converter">Converter</Link>
+        </div>
+      </nav>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/converter" component={Convertor} />
+      </Switch>
+    </div>
   )
 }
 
-export default App;
+export default App
